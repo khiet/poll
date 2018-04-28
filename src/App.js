e63732e19f389941a9f6d63a5a325054c42765c9
Khiet Le <khiet.le@ehochef.com>
2018-04-28 22:44:46 +0100

    add / and /poll

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import styles from './App.css';
 
 import Poll from './containers/Poll';
 
+import { Switch, Route, Link } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <h1>Ready to make a poll?</h1>
-        <Poll />
+        <Switch>
+          <Route path='/poll' component={Poll} />
+          <Route path='/' render={() => {
+            return(
+              <div>
+                <h1>Ready to make a poll?</h1>
+                <Link to='/poll'>CREATE POLL</Link>
+              </div>
+            );
+          }} />
+        </Switch>
       </div>
     );
   }
