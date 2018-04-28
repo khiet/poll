5c5f6c22d27447d37e99ebc14809b7d33e4d162d
Khiet Le <khiet.le@ehochef.com>
2018-04-28 20:14:59 +0100

    use css modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.css';
 
 import Poll from './containers/Poll';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.App}>
         <h1>Ready to make a poll?</h1>
         <Poll />
       </div>
