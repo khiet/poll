3b3244333a66757527d7662c1f924ef0cd9c2cb4
Khiet Le <khiet.le@ehochef.com>
2018-05-03 17:39:01 +0100

    use env to configure env specific variables

--- src/axios-polls.js
+++ src/axios-polls.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://poll-10186.firebaseio.com/'
+  baseURL: process.env.REACT_APP_DATABASE_URL
 });
 
 export default instance;
